0e773c7f2de28867cc4b318c4d3f1148bbb9df17
Alabi Fathiu <alabiabolore4@gmail.com>
2025-11-09 17:14:32 +0100

    second test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/AboloreDev/GoFarm
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"strings"
+)
+
+func Sound(s string) string{
+	return "When cockrel makes a sound it goes:" + strings.ToUpper(s)
+}
